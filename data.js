e0c09ba817ae4c8c1fe5b543037db8680bb3a581
vivek6159 <48536022+vrathod3010@users.noreply.github.com>
2019-09-22 12:59:31 +1000

    updates

---
+++ data.js
@@ -0,0 +1,79 @@
+const patient = {
+    "title": "Mr",
+    "firstName": "string",
+    "middleName": "string",
+    "lastName": "string",
+    "dateOfBirth": "2019-09-16T01:08:27.089Z",
+    "medicareNumber": 0,
+    "sex": "string",
+    "maritalStatus": "string",
+    "height": 0,
+    "id": "string"
+};
+
+const insurance = {
+    "insuranceProvider": "string",
+    "insuranceNumber": 0,
+    "effectiveDate": "2019-09-16T01:08:26.989Z",
+    "expiryDate": "2019-09-16T01:08:26.989Z",
+    "patientId": "string"
+};
+
+const contact = {
+    "address": "string",
+    "suburb": "string",
+    "postcode": 0,
+    "state": "string",
+    "country": "string",
+    "homePhone": 0,
+    "mobileNumber": 0,
+    "email": "string",
+    "emergencyContact": 0,
+    "emergencyPhone": 0,
+    "patientId": "string"
+};
+
+
+const sensorData = {
+    "pulseRate": 0,
+    "bloodGlucose": 0,
+    "bloodPressure": "string",
+    "weight": 0,
+    "bloodOxygen": 0,
+    "sleep": 0,
+    "physicalActivity": {},
+    "patientId": "string"
+};
+
+const carePlan = {
+    "monday": {
+        testTypes: [],
+        numberOfTests: 0
+    },
+    "tuesday": {
+        testTypes: [],
+        numberOfTests: 0
+    },
+    "wednesday": {
+        testTypes: [],
+        numberOfTests: 0
+    },
+    "thursday": {
+        testTypes: [],
+        numberOfTests: 0
+    },
+    "friday": {
+        testTypes: [],
+        numberOfTests: 0
+    },
+    "saturday": {
+        testTypes: [],
+        numberOfTests: 0
+    },
+    "sunday": {
+        testTypes: [],
+        numberOfTests: 0
+    },
+    "patientId": "string",
+    "id": "string"
+};
--- server/server.js
+++ server/server.js
@@ -23,6 +23,9 @@ app.start = function() {
   });
 };
 
+
+
+
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname, function(err) {
